08dfa881a637d561a5f805f783dbff57ad3b1775
Mael Astruc--Le Souder <mael.astruc@gmail.com>
2022-10-17 16:55:10 +0200

    fix devtools::check() warnings and notes

--- R-code/pkg/R/bartik.weight-package.r
+++ R-code/pkg/R/bartik.weight-package.r
@@ -1,5 +1,8 @@
 #' bartik.weight.
 #'
+#' Contains example datasets and a function for estimating Rotemberg
+#' weights outlined in Goldsmith-Pinkham, Sorkin and Swift (2018).
+#'
 #' @name bartik.weight
 #' @docType package
 NULL
